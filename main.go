eefa89f5934522d43a7f2ed79e527762cfcf2b29
Kesit Budi Kusumo <kesit@kernel.id>
2024-05-26 21:02:38 +0700

    feat: add solution using recursive
    O(n)

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := &TreeNode{
+		Val:  1,
+		Left: nil,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: nil,
+		},
+	}
+
+	output := preorderTraversal(input)
+	fmt.Println(output)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	result := append([]int{root.Val}, preorderTraversal(root.Left)...)
+	result = append(result, preorderTraversal(root.Right)...)
+	return result
 }
